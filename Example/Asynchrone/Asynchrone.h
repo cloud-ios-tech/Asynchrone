be100afe29c9f7942eaf1a5c7a4ca6b11e7934d4
Darya Tatarinova <d.tatarinova@corp.mail.ru>
2024-07-02 18:48:15 +0300

    carthage support

---
+++ Example/Asynchrone/Asynchrone.h
@@ -0,0 +1,18 @@
+//
+//  Asynchrone.h
+//  Asynchrone
+//
+//  Created by Darya Tatarinova on 02.07.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Asynchrone.
+FOUNDATION_EXPORT double AsynchroneVersionNumber;
+
+//! Project version string for Asynchrone.
+FOUNDATION_EXPORT const unsigned char AsynchroneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Asynchrone/PublicHeader.h>
+
+
